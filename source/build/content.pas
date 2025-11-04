4c1f1303029cd55585f46123a60392e5f15b543e
Jani Lähdesmäki <janilabo@gmail.com>
2025-11-04 19:25:31 +0200

    Latest MiMU

--- source/build/content.pas
+++ source/build/content.pas
@@ -1155,6 +1155,10 @@ begin
   AddFunction(@TPointArray_SortByIndex2, 'function TPointArray_SortByIndex(var arr: TPointArray; const aAscending: Boolean = True): Integer; overload;');
   AddFunction(@TPointArray_SortByScanline1, 'function TPointArray_SortByScanline(var arr: TPointArray; const aBounds: TBox; const aAscending: Boolean = True): Integer; overload;');
   AddFunction(@TPointArray_SortByScanline2, 'function TPointArray_SortByScanline(var arr: TPointArray; const aAscending: Boolean = True): Integer; overload;');
+  AddFunction(@TPointArray_ProximityCluster1, 'function TPointArray_ProximityCluster(const arr: TPointArray; const xRadius, yRadius: Integer; const method: Integer = 0): T2DPointArray; overload;');
+  AddFunction(@TPointArray_ProximityCluster2, 'function TPointArray_ProximityCluster(const arr: TPointArray; const radius: Double = 1.0; const metric: TDistanceMetric = dmEuclidean; const method: Integer = 0): T2DPointArray; overload;');
+  AddFunction(@TPointArray_Linked1, 'function TPointArray_Linked(const arr: TPointArray; const radius: Double = 1.0; const metric: TDistanceMetric = dmEuclidean; const check: Integer = 1): T2DPointArray; overload;');
+  AddFunction(@TPointArray_Linked2, 'function TPointArray_Linked(const arr: TPointArray; const xRadius, yRadius: Integer; const check: Integer = 1): T2DPointArray; overload;');
   
   AddFunction(@T2DPointArray_AllBounds1, 'function T2DPointArray_AllBounds(const arr: T2DPointArray): TBoxArray; overload;');
   AddFunction(@T2DPointArray_AllBounds2, 'function T2DPointArray_AllBounds(const arr: T2DPointArray; var aBounds: TBox): TBoxArray; overload;');
