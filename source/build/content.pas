b8c5ec04825bca25996695370e5e885df1dce8e8
Jani Lähdesmäki <janilabo@gmail.com>
2026-04-21 13:10:18 +0300

    Latest MiMU

--- source/build/content.pas
+++ source/build/content.pas
@@ -2460,6 +2460,7 @@ begin
   AddFunction(@TRangeArray_Boundaries, 'function TRangeArray_Boundaries(const arr: TRangeArray): TRange; overload;');
   AddFunction(@TRangeArray_Deconstruct, 'function TRangeArray_Deconstruct(const arr: TRangeArray): TIntegerArray; overload;');
   AddFunction(@TRangeArray_Disassemble, 'function TRangeArray_Disassemble(const arr: TRangeArray): TIntegerArray; overload;');
+  AddFunction(@TRangeArray_Spit, 'function TRangeArray_Spit(const arr: TRangeArray): TIntegerArray; overload;');
   AddFunction(@TRangeArray_Gapless, 'function TRangeArray_Gapless(const arr: TRangeArray): Boolean; overload;');
   AddFunction(@TRangeArray_Solid, 'function TRangeArray_Solid(const arr: TRangeArray): Boolean; overload;');
   AddFunction(@TRangeArray_Continuous, 'function TRangeArray_Continuous(const arr: TRangeArray): Boolean; overload;');
@@ -2467,6 +2468,11 @@ begin
   AddFunction(@TRangeArray_Cohesive, 'function TRangeArray_Cohesive(const arr: TRangeArray): Boolean; overload;');
   AddFunction(@TRangeArray_Integral, 'function TRangeArray_Integral(const arr: TRangeArray): Boolean; overload;');
   AddFunction(@TRangeArray_Monolithic, 'function TRangeArray_Monolithic(const arr: TRangeArray): Boolean; overload;');
+  AddFunction(@TRangeArray_Extent, 'function TRangeArray_Extent(const arr: TRangeArray): TRange; overload;');
+  AddFunction(@TRangeArray_Spannage, 'function TRangeArray_Spannage(const arr: TRangeArray): TRange; overload;');
+  AddFunction(@TRangeArray_MaxMin, 'function TRangeArray_MaxMin(const arr: TRangeArray): TRange; overload;');
+  AddFunction(@TRangeArray_MinMax, 'function TRangeArray_MinMax(const arr: TRangeArray): TRange; overload;');
+  AddFunction(@TRangeArray_Bounding, 'function TRangeArray_Bounding(const arr: TRangeArray; const rAscending: Boolean = True): TRange; overload;');
 
   AddFunction(@Var_TArray1, 'function Var_TArray(const v: Integer; const aSize: Integer = 1): TIntegerArray; overload;');
   AddFunction(@Var_TArray2, 'function Var_TArray(const v: Double; const aSize: Integer = 1): TDoubleArray; overload;');
